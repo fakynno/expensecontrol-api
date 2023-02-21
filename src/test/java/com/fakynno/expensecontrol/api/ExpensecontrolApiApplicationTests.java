f16663fd839946feb3ad0363ad3375646880903b
fakynno <fakynno@gmail.com>
2023-02-21 12:05:27 -0300

    Importe inicial do projeto

---
+++ src/test/java/com/fakynno/expensecontrol/api/ExpensecontrolApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fakynno.expensecontrol.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpensecontrolApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
