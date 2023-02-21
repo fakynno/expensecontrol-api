ce5c109ccb6134938fc607f5a17e0040e6117c1b
fakynno <fakynno@gmail.com>
2023-02-21 15:45:46 -0300

    Criacao da categoria controller e metodo para listagem

---
+++ src/main/java/com/fakynno/expensecontrol/api/resource/CategoriaResource.java
@@ -0,0 +1,23 @@
+package com.fakynno.expensecontrol.api.resource;
+
+import com.fakynno.expensecontrol.api.model.Categoria;
+import com.fakynno.expensecontrol.api.repository.CategoriaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/categorias")
+public class CategoriaResource {
+
+    @Autowired
+    private CategoriaRepository categoriaRepository;
+
+    @GetMapping
+    public List<Categoria> listar() {
+        return categoriaRepository.findAll();
+    }
+}
