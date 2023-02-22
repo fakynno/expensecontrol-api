22adca3568de035db18185c76eabe9c3da65c2af
fakynno <fakynno@gmail.com>
2023-02-22 11:54:09 -0300

    Add metodo para criar categoria

--- src/main/java/com/fakynno/expensecontrol/api/resource/CategoriaResource.java
+++ src/main/java/com/fakynno/expensecontrol/api/resource/CategoriaResource.java
@@ -3,10 +3,12 @@ package com.fakynno.expensecontrol.api.resource;
 import com.fakynno.expensecontrol.api.model.Categoria;
 import com.fakynno.expensecontrol.api.repository.CategoriaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import javax.servlet.http.HttpServletResponse;
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -19,5 +21,20 @@ public class CategoriaResource {
     @GetMapping
     public List<Categoria> listar() {
         return categoriaRepository.findAll();
+    }
+
+    @PostMapping
+    public ResponseEntity<Categoria> criar(@RequestBody Categoria categoria, HttpServletResponse response) {
+
+        Categoria categoriaSalva = categoriaRepository.save(categoria);
+
+        URI uri = ServletUriComponentsBuilder
+                .fromCurrentRequestUri()
+                .path("/{codigo]")
+                .buildAndExpand(categoriaSalva.getCodigo())
+                .toUri();
+        response.setHeader("Location", uri.toASCIIString());
+
+        return ResponseEntity.created(uri).body(categoriaSalva);
     }
 }
