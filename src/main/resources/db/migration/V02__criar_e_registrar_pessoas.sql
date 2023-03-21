1e7e5fff60c080b0af90ca7505ca8f1b2ebb43b0
fakynno <fakynno@gmail.com>
2023-03-21 19:56:40 -0300

    Add script para criar tabela pessoa

--- src/main/resources/db/migration/V02__criar_e_registrar_pessoas.sql
+++ src/main/resources/db/migration/V02__criar_e_registrar_pessoas.sql
@@ -1,16 +1,18 @@
 CREATE TABLE pessoa (
     codigo BIGINT(20) PRIMARY KEY AUTO_INCREMENT,
     nome VARCHAR(50) NOT NULL,
-    is_ativo BOOLEAN NOT NULL,
-    logradouro VARCHAR(50) NOT NULL,
-    numero VARCHAR(6) NOT NULL,
-    complemento VARCHAR(20) NOT NULL,
-    bairro VARCHAR(20) NOT NULL,
-    cep VARCHAR(8) NOT NULL,
-    cidade VARCHAR(20) NOT NULL,
-    estado VARCHAR(20) NOT NULL
+    ativo BOOLEAN NOT NULL,
+    endereco_logradouro VARCHAR(50) NOT NULL,
+    endereco_numero VARCHAR(6) NOT NULL,
+    endereco_complemento VARCHAR(20) NOT NULL,
+    endereco_bairro VARCHAR(20) NOT NULL,
+    endereco_cep VARCHAR(8) NOT NULL,
+    endereco_cidade VARCHAR(20) NOT NULL,
+    endereco_estado VARCHAR(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-INSERT INTO pessoa (nome, is_ativo, logradouro, numero, complemento, bairro, cep, cidade, estado) values
- ('fabio vasconcelos de aquino',true,'estrada dos guarantas','47','casa','chacara santa maria','06857810',
- 'itapecerica da serra','sao paulo');
+
+INSERT INTO pessoa (nome, ativo, endereco_logradouro, endereco_numero, endereco_complemento, endereco_bairro,
+endereco_cep, endereco_cidade, endereco_estado)
+values ('fabio vasconcelos de aquino',true,'estrada dos guarantas','47','casa','chacara santa maria','06857810',
+         'itapecerica da serra','sao paulo');
