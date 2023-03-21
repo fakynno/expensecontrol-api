84ec160c9b91bede445a2bf97f5b4a5fbdbcec69
fakynno <fakynno@gmail.com>
2023-03-21 19:12:08 -0300

    Add metodo para listar pessoas

--- src/main/java/com/fakynno/expensecontrol/api/resource/PessoaResource.java
+++ src/main/java/com/fakynno/expensecontrol/api/resource/PessoaResource.java
@@ -1,9 +1,24 @@
 package com.fakynno.expensecontrol.api.resource;
 
+import com.fakynno.expensecontrol.api.model.Pessoa;
+import com.fakynno.expensecontrol.api.repository.PessoaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/pessoas")
 public class PessoaResource {
+
+    @Autowired
+    private PessoaRepository pessoaRepository;
+
+    @GetMapping
+    public List<Pessoa> listar() {
+
+        return pessoaRepository.findAll();
+    }
 }
