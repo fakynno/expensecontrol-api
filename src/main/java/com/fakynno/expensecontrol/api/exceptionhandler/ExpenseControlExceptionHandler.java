8869b4dd70f90a0a4c5af0a18912025ec4a48994
fakynno <fakynno@gmail.com>
2023-03-21 17:24:59 -0300

    Add metodo para retornar uma lista de erros

--- src/main/java/com/fakynno/expensecontrol/api/exceptionhandler/ExpenseControlExceptionHandler.java
+++ src/main/java/com/fakynno/expensecontrol/api/exceptionhandler/ExpenseControlExceptionHandler.java
@@ -7,9 +7,13 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @ControllerAdvice
 public class ExpenseControlExceptionHandler extends ResponseEntityExceptionHandler {
@@ -27,6 +31,20 @@ public class ExpenseControlExceptionHandler extends ResponseEntityExceptionHandl
         return handleExceptionInternal(ex, new Erro(mensagemUsuario, mensagemDesenvolvedor), headers, HttpStatus.BAD_REQUEST, request);
     }
 
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
+                          HttpHeaders headers, HttpStatus status, WebRequest request) {
+
+        List<Erro> listaErros = novaListaDeErros();
+        return handleExceptionInternal(ex, listaErros, headers, HttpStatus.BAD_REQUEST, request);
+    }
+
+    private List<Erro> novaListaDeErros() {
+
+        List<Erro> erros = new ArrayList<>();
+
+        return erros;
+    }
 
     public static class Erro {
 
