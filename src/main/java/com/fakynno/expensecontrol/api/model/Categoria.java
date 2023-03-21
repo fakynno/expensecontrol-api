1062e3f32028250e44ec609fbb5480f3410ec0d4
fakynno <fakynno@gmail.com>
2023-03-20 22:41:12 -0300

    Add validacao para nao aceitar nulos

--- src/main/java/com/fakynno/expensecontrol/api/model/Categoria.java
+++ src/main/java/com/fakynno/expensecontrol/api/model/Categoria.java
@@ -1,16 +1,20 @@
 package com.fakynno.expensecontrol.api.model;
 
-import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
 import java.util.Objects;
 
 @Entity
-@Table(name = "categoria")
 public class Categoria {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long codigo;
 
+    @NotNull
     private String nome;
 
     public Long getCodigo() {
