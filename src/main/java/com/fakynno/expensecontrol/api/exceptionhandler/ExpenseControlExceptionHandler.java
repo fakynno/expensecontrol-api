d0a4a64780a3d9abae14033fca26d112851fc83f
fakynno <fakynno@gmail.com>
2023-03-21 17:34:39 -0300

    Tratamento para retornar lista de erros

--- src/main/java/com/fakynno/expensecontrol/api/exceptionhandler/ExpenseControlExceptionHandler.java
+++ src/main/java/com/fakynno/expensecontrol/api/exceptionhandler/ExpenseControlExceptionHandler.java
@@ -7,6 +7,8 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
@@ -37,13 +39,19 @@ public class ExpenseControlExceptionHandler extends ResponseEntityExceptionHandl
     protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
                           HttpHeaders headers, HttpStatus status, WebRequest request) {
 
-        List<Erro> listaErros = novaListaDeErros();
+        List<Erro> listaErros = novaListaDeErros(ex.getBindingResult());
         return handleExceptionInternal(ex, listaErros, headers, HttpStatus.BAD_REQUEST, request);
     }
 
-    private List<Erro> novaListaDeErros() {
+    private List<Erro> novaListaDeErros(BindingResult bindingResult) {
 
         List<Erro> erros = new ArrayList<>();
+
+        for (FieldError fieldError : bindingResult.getFieldErrors()) {
+            String mensagemUsuario = messageSource.getMessage(fieldError, LocaleContextHolder.getLocale());
+            String mensagemDesenvolvedor = fieldError.toString();
+            erros.add(new Erro(mensagemUsuario, mensagemDesenvolvedor));
+        }
 
         return erros;
     }
