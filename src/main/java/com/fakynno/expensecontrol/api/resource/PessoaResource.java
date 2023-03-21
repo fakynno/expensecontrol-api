5f34fcaab70eb7e18cc56086b484f383c33003bd
fakynno <fakynno@gmail.com>
2023-03-21 19:01:49 -0300

    Add anotacoes na classe pessoa

--- src/main/java/com/fakynno/expensecontrol/api/resource/PessoaResource.java
+++ src/main/java/com/fakynno/expensecontrol/api/resource/PessoaResource.java
@@ -1,4 +1,9 @@
 package com.fakynno.expensecontrol.api.resource;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/pessoas")
 public class PessoaResource {
 }
