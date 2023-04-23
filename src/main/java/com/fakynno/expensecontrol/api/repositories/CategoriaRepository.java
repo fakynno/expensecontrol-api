831377a89ef5e5cc8de2daff774c09d31b6ca8cc
fakynno <fakynno@gmail.com>
2023-04-23 18:27:12 -0300

    Refactoring nos packages

--- src/main/java/com/fakynno/expensecontrol/api/repositories/CategoriaRepository.java
+++ src/main/java/com/fakynno/expensecontrol/api/repositories/CategoriaRepository.java
@@ -1,6 +1,6 @@
-package com.fakynno.expensecontrol.api.repository;
+package com.fakynno.expensecontrol.api.repositories;
 
-import com.fakynno.expensecontrol.api.model.Categoria;
+import com.fakynno.expensecontrol.api.models.Categoria;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CategoriaRepository extends JpaRepository<Categoria, Long> {
