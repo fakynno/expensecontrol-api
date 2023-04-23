9862245b790634e87d6098698a85c289f8256dea
fakynno <fakynno@gmail.com>
2023-04-23 18:27:44 -0300

    Ajuste no metodo para salvar

---
+++ src/main/java/com/fakynno/expensecontrol/api/controllers/PessoaController.java
@@ -0,0 +1,52 @@
+package com.fakynno.expensecontrol.api.controllers;
+
+import com.fakynno.expensecontrol.api.events.RecursoCriadoEvent;
+import com.fakynno.expensecontrol.api.models.Pessoa;
+import com.fakynno.expensecontrol.api.repositories.PessoaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
+import java.net.URI;
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/pessoas")
+public class PessoaController {
+
+    @Autowired
+    private PessoaRepository pessoaRepository;
+
+    @Autowired
+    private ApplicationEventPublisher publisher;
+
+    @GetMapping
+    public List<Pessoa> listar() {
+
+        return pessoaRepository.findAll();
+    }
+
+    @PostMapping
+    public ResponseEntity<Pessoa> criarPessoa(@Valid @RequestBody Pessoa pessoa, HttpServletResponse response) {
+
+        Pessoa novaPessoa = pessoaRepository.save(pessoa);
+
+        publisher.publishEvent(new RecursoCriadoEvent(this, response, novaPessoa.getCodigo()));
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(novaPessoa);
+    }
+
+    @GetMapping("/{codigo}")
+    public ResponseEntity<?> buscarPessoaPeloCodigo(@PathVariable Long codigo) {
+
+        Optional<Pessoa> pessoa = pessoaRepository.findById(codigo);
+
+        return pessoa.isEmpty() ? ResponseEntity.notFound().build() : ResponseEntity.ok(pessoa);
+    }
+}
