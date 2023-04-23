cfc1a135f36d191d8e71926c0d138390d5f3e48c
fakynno <fakynno@gmail.com>
2023-04-23 18:26:08 -0300

    Refactoring nos packages

--- src/main/java/com/fakynno/expensecontrol/api/exceptions/exceptionhandler/ExpenseControlExceptionHandler.java
+++ src/main/java/com/fakynno/expensecontrol/api/exceptions/exceptionhandler/ExpenseControlExceptionHandler.java
@@ -1,4 +1,4 @@
-package com.fakynno.expensecontrol.api.exceptionhandler;
+package com.fakynno.expensecontrol.api.exceptions.exceptionhandler;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
