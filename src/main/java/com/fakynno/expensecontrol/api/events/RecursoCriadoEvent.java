b88322ee431a21848305955c122286f397c35bc5
fakynno <fakynno@gmail.com>
2023-04-23 18:40:56 -0300

    Ajustes nos imports

--- src/main/java/com/fakynno/expensecontrol/api/events/RecursoCriadoEvent.java
+++ src/main/java/com/fakynno/expensecontrol/api/events/RecursoCriadoEvent.java
@@ -3,13 +3,15 @@ package com.fakynno.expensecontrol.api.events;
 import org.springframework.context.ApplicationEvent;
 
 import javax.servlet.http.HttpServletResponse;
+import java.io.Serial;
 
 public class RecursoCriadoEvent extends ApplicationEvent {
 
+    @Serial
     private static final long serialVersionUID = 1L;
 
-    private HttpServletResponse response;
-    private Long codigo;
+    private final HttpServletResponse response;
+    private final Long codigo;
     public RecursoCriadoEvent(Object source, HttpServletResponse response, Long codigo) {
         super(source);
         this.response = response;
